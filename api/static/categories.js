57a9884ff3a0349b938a5dd0a2ebfd86c862223f
fid <femiomoj2001@gmail.com>
2025-03-21 13:10:51 +0100

    testing

---
+++ api/static/categories.js
@@ -0,0 +1,15 @@
+var swiper2 = new Swiper('.swiper-container2', {
+    grabCursor: true,
+    centeredSlides: false,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: false,
+    freeMode: true,
+    spaceBetween: 0,
+});
